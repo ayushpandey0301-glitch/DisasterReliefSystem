b591b700f70d8880e6dc91d2e54f8c884fe32119
AYUSH PANDEY <ayushpandey0301@gmail.com>
2026-09-06 02:17:45 +0530

    Fix database connection for Render Aiven MySQL

--- utils/database.py
+++ utils/database.py
@@ -9,11 +9,34 @@ from config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
 
 def get_db_connection():
 
-    connection = mysql.connector.connect(
-        host=DB_HOST,
-        user=DB_USER,
-        password=DB_PASSWORD,
-        database=DB_NAME
-    )
+    # =====================================================
+    # LOCAL MYSQL
+    # =====================================================
+
+    if DB_HOST in ["localhost", "127.0.0.1"]:
+
+        connection = mysql.connector.connect(
+            host=DB_HOST,
+            port=3306,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            database=DB_NAME
+        )
+
+    # =====================================================
+    # REMOTE MYSQL - AIVEN / RENDER
+    # =====================================================
+
+    else:
+
+        connection = mysql.connector.connect(
+            host=DB_HOST,
+            port=10732,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            database=DB_NAME,
+            ssl_verify_cert=False,
+            ssl_verify_identity=False
+        )
 
     return connection
